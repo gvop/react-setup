4b5d80469a4ffcd7507fa2ab9d8250a06de91d7f
Guus van Ooijen <guusvanooijen@gmail.com>
2016-07-25 15:46:11 +0100

    Second video added

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>
+    let txt = this.props.txt
+    let cat = this.props.cat
+    return  <h1>{txt}, {cat}</h1>
   }
 }
 
-export default App
+App.PropTypes = {
+  txt: React.PropTypes.String,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'This is the default text'
+}
+
+ReactDOM.render(
+  <App cat={5} txt="This is another text" />,
+  document.getElementById('app')
+);
